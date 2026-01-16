077ed26767e365fd785cb0eb6a449456150cf54c
Jian Hong Li <lijh0901@gmail.com>
2026-01-16 00:46:31 -0500

    fix: git plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,7 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
-        lazy = true,
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
             require("gitsigns").setup({
                 current_line_blame = true,
